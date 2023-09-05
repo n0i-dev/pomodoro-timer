b7032134d927889f43e930181fb5d0d2c7abbc05
n0i <n0i@protonmail.com>
2023-09-04 22:22:01 -0400

    Added tab for break timer

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const resetButton = document.getElementById("reset") ;
 const timerSection = document.getElementById("timer");
 const settingsButton = document.getElementById("settings");
 const timerInput = document.getElementById("timer-length");
+const breakTimerInput = document.getElementById("break-timer-length");
 const modal = document.querySelector(".modal");
+let workTabSelected = true;
 
 timerSection.innerHTML = timer.getTimeFormatted();
 
@@ -34,13 +36,53 @@ document.getElementById("settings").addEventListener("click", () => {
 
 resetButton.addEventListener("click", resetTimer);
 settingsButton.addEventListener("click", () => {pauseTimer("Resume")});
-
 timerInput.value = Math.floor(timer.getInitialTime() / 60);
 timerInput.addEventListener("input", (event) => {
+  if (workTabSelected) {
     timer.setInitialTime(event.target.value);
     timer.resetTimer();
     timerSection.innerHTML = timer.getTimeFormatted();
+  }
 });
+
+breakTimerInput.value = Math.floor(getBreakTimeLength());
+breakTimerInput.addEventListener("input", (event) => {
+  localStorage.setItem("breakLengthInMinutes", event.target.value);
+
+  if (!workTabSelected) {
+    timer.setInitialTime(event.target.value);
+    timer.resetTimer();
+    timerSection.innerHTML = timer.getTimeFormatted();
+  }
+});
+
+document.querySelectorAll("ul > li").forEach((element) => {
+  onTabClick(element);
+});
+
+function onTabClick(element) {
+  element.addEventListener("click", () => {
+    let innerText = element.firstElementChild.textContent;
+    pauseTimer("Start");
+    workTabSelected = innerText == "Work";
+
+    if (innerText == "Work") {
+      timer.setInitialTime(timerInput.value);
+    }
+    else {
+      timer.setInitialTime(getBreakTimeLength());
+    }
+
+    timer.resetTimer();
+    timerSection.innerHTML = timer.getTimeFormatted();
+    document.querySelectorAll("ul > li.is-active").forEach((element) => element.classList.remove("is-active"));
+    element.classList.add("is-active");
+  });
+}
+
+function getBreakTimeLength() {
+  return localStorage.getItem("breakLengthInMinutes") ?? 5;
+}
 
 function startTimer() {
   console.log("Timer has started!");
